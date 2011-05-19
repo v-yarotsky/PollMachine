7f2e5c1d1613f83b4c6bc0e02098d4dfd0b392c4
v.yarotsky <v.yarotsky@itransition.com>
2011-05-20 02:34:29 +0300

    styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def button_link(text, url, css_class = "", options = {})
+    options.reverse_merge!({ :title => text, :class => "button #{css_class}" })
+    link_to(text, url, options).html_safe
+  end
 end
